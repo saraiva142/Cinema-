f61065971e1faf6cada530d781346b8adc44e5fb
saraiva142 <saraivajandrade@gmail.com>
2025-04-08 08:35:03 -0300

    Aula e Colocando Modal e Regras 🚀

---
+++ controller/filmesController.js
@@ -0,0 +1,17 @@
+import { Filme } from "../model/filmesModel.js";
+
+class FilmesController {
+  constructor() {
+    this.listaFilmes = [];
+    this.idEmEdicao = NULL;
+    this.init();
+  }
+
+  init() {
+    
+    const btnNovoFilme = document.getElementById("btn-salvar");
+    
+
+  }
+
+}
---
+++ model/filmesModel.js
@@ -0,0 +1,58 @@
+export class Filme {
+  constructor(id, titulo, genero, descricao, classificacaoIndicativa, duracao, data ) {
+    this.id = id;
+    this.titulo = titulo;
+    this.genero = genero;
+    this.descricao = descricao;
+    this.classificacaoIndicativa = classificacaoIndicativa;
+    this.duracao = duracao;
+    this.data = data;
+  }
+  
+  getId() {
+    return this.id;
+  }
+  getTitulo() {
+    return this.titulo;
+  }
+  
+  getGenero() {
+    return this.genero;
+  }
+  getDescricao() {
+    return this.descricao;
+  }
+  getClassificacaoIndicativa() {
+    return this.classificacaoIndicativa;
+  }
+  getDuracao() {
+    return this.duracao;
+  }
+  getData() {
+    return this.data;
+  }
+  setId(id) {
+    this.id = id;
+  }
+  setTitulo(titulo) {
+    this.titulo = titulo;
+  }
+  setGenero(genero) {
+    this.genero = genero;
+  }
+  setDescricao(descricao) {
+    this.descricao = descricao;
+  }
+  setClassificacaoIndicativa(classificacaoIndicativa) {
+    this.classificacaoIndicativa = classificacaoIndicativa;
+  }
+  setDuracao(duracao) {
+    this.duracao = duracao;
+  }
+  setData(data) {
+    this.data = data;
+  }
+  toString() {
+    return `Filme: ${this.titulo}, Genero: ${this.genero}, Descricao: ${this.descricao}, Classificacao Indicativa: ${this.classificacaoIndicativa}, Duracao: ${this.duracao}, Data: ${this.data}`;
+  }
+}
